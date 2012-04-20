09aefd39c3548ccf6f19ba748b1ee7d591b34c6c
ratdog <ratdog@monalisa.(none)>
2012-04-20 14:35:45 -0500

    Multi-level validation, full form validation, addition of valid method for form onsubmit()

--- README.md
+++ README.md
@@ -7,12 +7,55 @@ Spitshine requires the use of Bootstrap 2.0's new control-group structure and sy
 
 ```javascript
 	$(document).ready(function() {
-				
-		$('#form').spitshine();
 					
+		$('#form').spitshine();
+		$('#form').submit(function() { return $('#form').valid(); });
+		
 	});
 ```
 
 ```html
-	<input class="valid-required valid-email" id="email" name="email" type="text" />
+	<form id="form">
+		<div class="control-group">
+			<div class="input">
+				<input class="valid-required valid-email" id="email" name="email" type="text" />
+				<span class="help-inline"></span>
+			</div>
+		</div>
+	</form>	
 ```
+
+Available Methods
+-------------------------
+
+* spitshine (interface/driver method)
+
+	1) () - Null/Empty (Initialization/Constructor)
+	2) ('valid') (returns true/false depending on entire form validation)
+
+Available Validation Classes
+-------------------------
+
+* valid-required
+
+	1) Checks input type=text for val() === ''
+	
+	2) Checks input type=checkbox for is:checked
+	
+	3) Checks input type=select for is:selected
+	
+* valid-email
+
+	1) Simplified email regex (/\S+@\S+\.\S+/) please confirm email, do not rely on validation.
+	
+* valid-numeric
+
+	1) Checks for a numeric only string 
+	
+* valid-alpha
+
+	1) Checks for alphabetic only entry
+
+* valid-length-(n)
+
+	1) Validates exact length (valid-length-5 for zipcodes)
--- spitshine.js
+++ spitshine.js
@@ -2,36 +2,144 @@
 
 	"use strict"
 
-	var methods = {
+	var errors 			= false;
+	
+	var methods 		= {
 		
-		init : function( options ) {
+		init : function(options) {
 			
 			return $('#' + $(this).attr('id') + ' input').each(function() {
 				
-				if ($(this).hasClass('valid-required')) {
-				
-					$(this).bind('focusout', methods.valid_required);
+				var _this = this;
+								
+				$($(_this).attr('class').split(' ')).each(function() {
+				 	
+					if (this !== '') {
 					
-				}
+						if (this.substring(0,6) === 'valid-') {
+							
+							$(_this).bind('focusout', methods.validate_field);
+							
+						}
+						
+					}
+					
+				});
 				
 				
 			});
 
 		},
 		
-		destroy : function( ) {
-
-			//return this.each(function(){
-				//$(window).unbind('.tooltip');
-			//})
-
+		destroy : function() {},
+		
+		valid : function(opts) {
+			
+			$('#' + $(this).attr('id') + ' input').each(function() {
+				 
+				methods.validate_field(this); 
+				
+			});
+			
+			return !errors;
+			
 		},
 		
-		valid_required : function( ) { 
+		validate_field : function (selector) {
+			
+			var valid 		= true;
+			
+			if (selector === undefined) { selector = this; }
+			
+			$($(selector).attr('class') !== undefined && $(selector).attr('class').split(' ')).each(function() {
+				 	
+				if (valid === true && this !== '') {
+				
+					if (this.substring(0,6) === 'valid-') {
+						 
+						if (typeof methods[this.replace('-', '_')] === 'function') {
+						
+							if (methods[this.replace('-', '_')](selector) === false) { errors = true; valid = false; };
+							
+						}
+												
+					}
+					
+				}
+				
+			});
+			
+			if (valid === true) { errors = false; }
+			
+		},
+
+		valid_required : function(field) { 
 		 
-			if ($(this).attr('type') == 'text' && $(this).val() === '') { $(this).parent().parent().addClass('error'); }
-			else if ($(this).attr('type') == 'text' && $(this).val() !== '') { $(this).parent().parent().removeClass('error'); $(this).parent().parent().removeClass('success'); }
+		 	var selector 	= $(field).parent().parent();
 		 
+			if ($(field).attr('type') === 'text' && $(field).val() === '') { selector.removeClass('success'); selector.addClass('error'); return false; }
+			else if ($(field).attr('type') === 'text' && $(field).val() !== '') { selector.removeClass('error'); selector.addClass('success'); }
+		 
+		 	if ($(field).attr('type') === 'select' && !$(field).is(':selected')) { selector.addClass('error'); return false; }
+			else if ($(field).attr('type') === 'select' && $(field).is(':selected')) { selector.removeClass('error'); selector.addClass('success'); }
+			
+			if ($(field).attr('type') === 'checkbox' && !$(field).is(':checked')) { selector.addClass('error'); return false; }
+			else if ($(field).attr('type') === 'checkbox' && $(field).is(':checked')) { selector.removeClass('error'); selector.addClass('success'); }
+		 
+		},
+		
+		valid_email : function(field) {
+		 	
+			var selector 	= $(field).parent().parent();
+			var regex 		= /\S+@\S+\.\S+/;
+			
+			if (!regex.test(jQuery.trim($(field).val()))) { selector.addClass('error'); selector.removeClass('success'); return false; }
+			else { selector.removeClass('error'); selector.addClass('success'); }
+	
+		},
+		
+		valid_numeric : function() {
+			
+			var _this 		= this;
+			var selector 	= $(this).parent().parent();
+			var regex = /^[-]?[0-9]+[\.]?[0-9]+$/;
+			
+			if (!regex.test(jQuery.trim($(this).val()))) { selector.addClass('error'); selector.removeClass('success'); return false; }
+			else { selector.removeClass('error'); selector.addClass('success'); }
+			
+		},
+		
+		valid_alpha : function() {
+			
+			var _this 		= this;
+			var selector 	= $(this).parent().parent();
+			var regex 		= /^[a-zA-Z]+$/;
+			
+			if (!regex.test(jQuery.trim($(this).val()))) { selector.addClass('error'); selector.removeClass('success'); return false; }
+			else { selector.removeClass('error'); selector.addClass('success'); }
+			
+		},
+		
+		valid_length : function() {
+			
+			var _this 		= this;
+			var selector 	= $(this).parent().parent();
+			
+			$($(this).attr('class').split(' ')).each(function() {
+				 
+				if (this !== '') {
+					
+					if (this.substring(0,13) == 'valid-length-') {
+						
+						if (jQuery.trim($(this).val()).length !== this.substring(13)) { selector.addClass('error'); selector.removeClass('success'); return false; }
+						else { selector.removeClass('error'); selector.addClass('success'); }
+						
+					}
+					
+				}
+				
+    		});
+			
 		}
 		
   	};
@@ -40,9 +148,9 @@
   
 		if (methods[method]) {
 			
-			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 			
-		} else if (typeof method === 'object' || ! method) {
+		} else if (typeof method === 'object' || !method) {
 			
 			return methods.init.apply(this, arguments);
 			
@@ -54,14 +162,4 @@
 
   };
   
-}( window.jQuery );
-
-
-
-function _validate_email(val) {
-
-	var regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;  
-	return regex.test(val);
-	  
-}  
-     
+}(window.jQuery);
